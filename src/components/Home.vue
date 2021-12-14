e49850f400ea6601c164bb456251fc2b08163bc0
DIRTYBOTTLE <62377297+DIRTYBOTTLE@users.noreply.github.com>
2021-12-15 00:55:25 +0800

    Update Home.vue

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -57,7 +57,6 @@ import { defineComponent, ref } from "vue";
 import { useRouter } from "vue-router";
 // import { useStore } from "vuex";
 // import Cookies from "js-cookie";
-// testtttt
 export default defineComponent({
 	setup() {
 		const router = useRouter();
